454080913646cc0cb12a469a707a005e28202024
Ameen-KM10 <ameenkaroli@gmail.com>
2025-06-25 22:50:53 +0530

    landing page works #1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import Particles from './Particles';
 import DecryptedText from './DecryptedText'
 import {useGSAP} from "@gsap/react";
 import {gsap} from "gsap";
@@ -10,28 +10,54 @@ const Home = () => {
 
   useGSAP(()=>{
     gsap.from(title.current, {
-      scale:0,
-      x:500,
+      scale:1,
+      x:100,
       duration:1,
     })
   })
 
   return (
     <>
-    <div className="h-screen  flex justify-center items-center">
-      <div ref={title}>Ameen KM</div>
-      {/* <div className='text-8xl' style={{ marginTop: '4rem' }}>
+    {/* <div className="h-screen px-40 bg-bunker-950 flex flex-col justify-center items-start">
+      <div ref={title} className="font-['SFpro-reg'] text-9xl text-amber-50 leading-5">Ameen KM</div>
+      <div className="font-['JBmono-reg'] text-4xl text-white " style={{ marginTop: '4rem' }}>
         <DecryptedText
-          text="This text animates when in view"
+          text="Web Developer, Data Scientist"
           speed={30}
           sequential={true}
           maxIterations={10}
           animateOn="view"
           revealDirection="start"
         />
-      </div> */}
-        
+      </div>
+    </div> */}
+    {/* <div className='stack w-full'> */}
+    <div className='h-screen px-40 bg-dark-purple relative flex flex-col items-center justify-center' >
+      <div className='absolute h-screen w-screen' >
+        <Particles
+          particleColors={['#ffffff','#ffffff']}
+          particleCount={300}
+          particleSpread={15}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={true}
+          />
+      </div>
+      <div ref={title} className="font-['SFpro-reg'] text-9xl text-amber-50 leading-5 ">Ameen KM</div>
+      <div className="font-['JBmono-reg'] text-4xl text-white " style={{ marginTop: '4rem' }}>
+        <DecryptedText
+          text="Web Developer, Data Scientist"
+          speed={30}
+          sequential={true}
+          maxIterations={10}
+          animateOn="view"
+          revealDirection="start"
+          />
+      </div>
     </div>
+{/* </div> */}
     </>
   )
 }
