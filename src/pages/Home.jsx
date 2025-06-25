d47af1b78687ffef0547c3437459529193e93bea
Ameen-KM10 <ameenkaroli@gmail.com>
2025-06-25 13:39:11 +0530

    gsap

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import DecryptedText from './DecryptedText'
+import {useGSAP} from "@gsap/react";
+import {gsap} from "gsap";
+import {useRef} from "react";
 
 const Home = () => {
+
+  const title = useRef()
+
+  useGSAP(()=>{
+    gsap.from(title.current, {
+      scale:0,
+      x:500,
+      duration:1,
+    })
+  })
+
   return (
     <>
     <div className="h-screen  flex justify-center items-center">
-      <div className='text-8xl' style={{ marginTop: '4rem' }}>
+      <div ref={title}>Ameen KM</div>
+      {/* <div className='text-8xl' style={{ marginTop: '4rem' }}>
         <DecryptedText
           text="This text animates when in view"
           speed={30}
@@ -14,7 +29,7 @@ const Home = () => {
           animateOn="view"
           revealDirection="start"
         />
-</div>
+      </div> */}
         
     </div>
     </>
